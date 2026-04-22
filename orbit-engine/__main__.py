4b3fd100cef49c015a064913117fb3ef9b7dcb55
Nathan Jacobson <nathan@nathanjacobson.ca>
2026-04-22 14:13:29 -0700

    got basic window working

--- orbit-engine/__main__.py
+++ orbit-engine/__main__.py
@@ -2,5 +2,28 @@
 Orbit Engine
 """
 
+import wgpu
+import logging
+import sys
+from rendercanvas.auto import RenderCanvas, loop
+
+WIDTH, HEIGHT = 1280, 720
+
 if __name__ == "__main__":
-    pass
+    logger = logging.getLogger("wgpu")
+    logger.setLevel(logging.INFO)
+
+    ch = logging.StreamHandler()
+    ch.setLevel(logging.INFO)
+    logger.addHandler(ch)
+
+    canvas = RenderCanvas(title="OrbitEngine", size=(WIDTH, HEIGHT))
+
+    adapter = wgpu.gpu.request_adapter_sync(power_preference="high-performance")
+    device = adapter.request_device_sync()
+
+    present_context = canvas.get_wgpu_context()
+    render_texture_format = present_context.get_preferred_format(device.adapter)
+    present_context.configure(device=device, format=render_texture_format)
+
+    loop.run()
