65257b9b6ffc771eaef2a1c68a7616a881873781
Nathan Jacobson <nathan@nathanjacobson.ca>
2026-04-22 14:01:25 -0700

    starting over

--- orbit-engine/__main__.py
+++ orbit-engine/__main__.py
@@ -1,14 +1,6 @@
 """
 Orbit Engine
 """
-from .rendering import setup_drawing_sync
 
 if __name__ == "__main__":
-    from rendercanvas.auto import RenderCanvas, loop
-
-    canvas = RenderCanvas(size=(1280, 720), title="Orbit Engine")
-    context = canvas.get_wgpu_context()
-
-    draw_frame = setup_drawing_sync(context)
-    canvas.request_draw(draw_frame)
-    loop.run()
+    pass
