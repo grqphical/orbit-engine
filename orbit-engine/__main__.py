5181e686b3edf20a21b5b81d0d816299102e220f
Nathan Jacobson <nathan@nathanjacobson.ca>
2026-04-22 18:04:19 -0700

    added flight camera

--- orbit-engine/__main__.py
+++ orbit-engine/__main__.py
@@ -1,23 +1,24 @@
 import pygfx as gfx
-from rendercanvas.auto import RenderCanvas
+from rendercanvas.auto import RenderCanvas, loop
 
 canvas = RenderCanvas(size=(1280, 720), title="OrbitEngine")
+renderer = gfx.renderers.WgpuRenderer(canvas)
 
 scene = gfx.Scene()
 
 scene.add(gfx.AmbientLight())
 scene.add(gfx.DirectionalLight())
 
-camera = gfx.PerspectiveCamera(45, 16 / 9)
+camera = gfx.PerspectiveCamera(45, 1)
 camera.local.z = 400
-scene.add(camera)
+
+controller = gfx.FlyController(camera, speed=80, register_events=renderer)
 
 geometry = gfx.box_geometry(200, 200, 200)
 material = gfx.MeshPhongMaterial(color="#336699")
 cube = gfx.Mesh(geometry, material)
 scene.add(cube)
 
-camera.show_object(cube, view_dir=(-1, -1, -1), match_aspect=True)
-
 if __name__ == "__main__":
-    gfx.show(scene, canvas=canvas)
+    canvas.request_draw(lambda: renderer.render(scene, camera))
+    loop.run()
